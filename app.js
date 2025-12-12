dbf54f3456dcd7d030323a7cbdbc849ab4fec62e
osinsomkuwar-27 <osinsomkuwar26@gmail.com>
2025-12-12 08:39:43 +0530

    created listing model

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const Listing = require('./models/listing');
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderinn";
 
@@ -17,6 +18,20 @@ async function main(){
 
 app.get("/", (req, res) => {
     res.send("Welcome to WanderInn");
+});
+
+app.get("/testListing", async (req, res) => {
+    let sampleListing = new Listing({
+        title: "Cozy Cottage",
+        description: "A cozy cottage in the countryside.",
+        image: " ",
+        price: 1200,
+        location: "Calangute, Goa",
+        country: "India"
+    });
+    await sampleListing.save()
+    console.log("sample was saved");
+    res.send("Sample listing saved successfully!");
 });
 
 app.listen(8080,() => {
