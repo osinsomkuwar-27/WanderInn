35fdf79e90ffd36b4025fb3be593a259f19602c5
osinsomkuwar-27 <osinsomkuwar26@gmail.com>
2025-12-12 09:29:30 +0530

    created index route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const Listing = require('./models/listing');
+const path = require('path');
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderinn";
 
@@ -16,23 +17,33 @@ async function main(){
     await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
     res.send("Welcome to WanderInn");
 });
 
-app.get("/testListing", async (req, res) => {
-    let sampleListing = new Listing({
-        title: "Cozy Cottage",
-        description: "A cozy cottage in the countryside.",
-        image: " ",
-        price: 1200,
-        location: "Calangute, Goa",
-        country: "India"
-    });
-    await sampleListing.save()
-    console.log("sample was saved");
-    res.send("Sample listing saved successfully!");
+//index route to show all listings
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {listings: allListings});
 });
+
+
+// app.get("/testListing", async (req, res) => {
+//     let sampleListing = new Listing({
+//         title: "Cozy Cottage",
+//         description: "A cozy cottage in the countryside.",
+//         image: " ",
+//         price: 1200,
+//         location: "Calangute, Goa",
+//         country: "India"
+//     });
+//     await sampleListing.save()
+//     console.log("sample was saved");
+//     res.send("Sample listing saved successfully!");
+// });
 
 app.listen(8080,() => {
     console.log("Server is running on port 8080");
