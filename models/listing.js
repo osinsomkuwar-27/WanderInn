f4b7fd3ced3729ea5adb317da7b1c2d74aed03e8
osinsomkuwar-27 <osinsomkuwar26@gmail.com>
2025-12-12 08:52:35 +0530

    Initialized database

--- models/listing.js
+++ models/listing.js
@@ -1,26 +1,29 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const imageSchema = new Schema({
+  filename: String,
+  url: String,
+});
+
 const listingSchema = new Schema({
   title: {
     type: String,
     required: true,
   },
   description: String,
+
   image: {
-    type: String,
-    default:
-      "https://www.freepik.com/free-ai-image/breathtaking-view-natural-beach-landscape_266519540.htm#fromView=keyword&page=1&position=0&uuid=7ed9e903-8009-468a-a8df-1261c42722b5&query=Beautiful+scenery",
-    set: (v) =>
-      v === " "
-        ? "https://www.freepik.com/free-ai-image/breathtaking-view-natural-beach-landscape_266519540.htm#fromView=keyword&page=1&position=0&uuid=7ed9e903-8009-468a-a8df-1261c42722b5&query=Beautiful+scenery"
-        : v,
+    type: imageSchema,
+    default: {
+      filename: "default",
+      url: "https://www.freepik.com/free-ai-image/breathtaking-view-natural-beach-landscape_266519540.htm#fromView=keyword&page=1&position=0&uuid=7ed9e903-8009-468a-a8df-1261c42722b5&query=Beautiful+scenery",
+    },
   },
+
   price: Number,
   location: String,
   country: String,
 });
 
-const Listing = mongoose.model("Listing", listingSchema);
-
-module.exports = Listing;
+module.exports = mongoose.model("Listing", listingSchema);
