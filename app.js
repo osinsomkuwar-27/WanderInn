9181450467f553dc81904fa7645730f60e20a43b
osinsomkuwar-27 <osinsomkuwar26@gmail.com>
2025-12-13 09:40:07 +0530

    READ (Show Route)

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ async function main(){
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.send("Welcome to WanderInn");
@@ -30,6 +31,12 @@ app.get("/listings", async (req, res) => {
     res.render("listings/index.ejs", {listings: allListings});
 });
 
+//show route
+app.get("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
+});
 
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
