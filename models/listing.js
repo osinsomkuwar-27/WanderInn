f86cb7731bf99471cd0b9bc0e99e064ce5b694ed
osinsomkuwar-27 <osinsomkuwar26@gmail.com>
2025-12-18 17:10:59 +0530

    Styling show listing

--- models/listing.js
+++ models/listing.js
@@ -15,10 +15,10 @@ const listingSchema = new Schema({
 
   image: {
     type: imageSchema,
-    default: {
-      filename: "default",
-      url: "https://www.freepik.com/free-ai-image/breathtaking-view-natural-beach-landscape_266519540.htm#fromView=keyword&page=1&position=0&uuid=7ed9e903-8009-468a-a8df-1261c42722b5&query=Beautiful+scenery",
-    },
+    default: 
+      "https://images.unsplash.com/photo-1506744038136-46273834b3fbhttps://images.unsplash.com/photo-1501785888041-af3ef285b470?auto=format&fit=crop&w=1200&q=80",
+    set:(v) =>
+      v === " " ? "https://images.unsplash.com/photo-1506744038136-46273834b3fbhttps://images.unsplash.com/photo-1501785888041-af3ef285b470?auto=format&fit=crop&w=1200&q=80" : v,
   },
 
   price: Number,
