ec89f40cb3b99dd52dab2fafc759ad101f750dc3
osinsomkuwar-27 <osinsomkuwar26@gmail.com>
2026-01-19 14:25:21 +0530

    added error.ejs which is a error page which gets rendered if there is any error

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) =>{
     let {statusCode = 500, message = "Something went wrong"} = err;
-    res.status(statusCode).send(message);
+    res.status(statusCode).render("listings/error.ejs", { message });
 });
 
 app.listen(8080,() => {
