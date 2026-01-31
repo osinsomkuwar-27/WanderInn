570696a16d8b33989efab44439f873e5ffd607d7
osinsomkuwar-27 <osinsomkuwar26@gmail.com>
2026-01-31 13:55:46 +0530

    created review model which containes the comment, rating and the createdat date

--- models/listing.js
+++ models/listing.js
@@ -15,15 +15,18 @@ const listingSchema = new Schema({
 
   image: {
     type: imageSchema,
-    default: 
+    default:
       "https://images.unsplash.com/photo-1506744038136-46273834b3fbhttps://images.unsplash.com/photo-1501785888041-af3ef285b470?auto=format&fit=crop&w=1200&q=80",
-    set:(v) =>
-      v === " " ? "https://images.unsplash.com/photo-1506744038136-46273834b3fbhttps://images.unsplash.com/photo-1501785888041-af3ef285b470?auto=format&fit=crop&w=1200&q=80" : v,
+    set: (v) =>
+      v === " "
+        ? "https://images.unsplash.com/photo-1506744038136-46273834b3fbhttps://images.unsplash.com/photo-1501785888041-af3ef285b470?auto=format&fit=crop&w=1200&q=80"
+        : v,
   },
 
   price: Number,
   location: String,
   country: String,
+  reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
 });
 
 module.exports = mongoose.model("Listing", listingSchema);
